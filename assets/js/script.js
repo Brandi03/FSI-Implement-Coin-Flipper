9e225533273e6fdfd1fe0742df20925b00cde419
Brandi Parsons <branpar2@gmail.com>
2022-08-22 20:02:00 -0400

    coin flip

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+
 
     // TODO: Add event listener and handler for flip and clear buttons
-
+document.getElementById('flip').addEventListener('click', function() {
+     
+    if (Math.random() < o.5) {
+         document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+    } else {
+         document.getElementById('penny-image').src = 'assets/images/penny-tails.jpg'
+    }
+})
     // Flip Button Click Handler
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
